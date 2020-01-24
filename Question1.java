689efe7a62194ef303d1727dba3e8a26baae86d9
MichaelTzur <59913971+MichaelTzur@users.noreply.github.com>
2020-01-24 11:20:50 +0200

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,66 @@
+public class Question1 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {0,1,2,4,4,4,4,4,4,4};
+        int num = 4;
+
+        int lowerLimit = lowerLimit(arr, num);
+        int upperLimit = upperLimit(arr, num);
+
+        int AmountOfAppearances = upperLimit - lowerLimit + 1;
+
+        System.out.println(AmountOfAppearances);
+    }
+
+    public static int upperLimit(int[] arr, int num) {
+
+        int start = 0, last = arr.length - 1;
+
+        while (start <= last) {
+            int middle = (start + last) / 2;
+
+            if (arr[middle] > num) {
+                last = middle - 1;
+            }
+            else if (arr[middle] < num) {
+                start = middle + 1;
+            }
+            else {
+                start = middle;
+
+                if (arr[start] == arr[last]) {
+                    return last;
+                }
+                if (last - start == 1) {
+                    return start;
+                }
+            }
+        }
+        return -2;
+    }
+
+    public static int lowerLimit(int[] arr, int num) {
+
+        int start = 0, last = arr.length - 1;
+
+        while (start <= last) {
+            int middle = (start + last) / 2;
+
+            if (arr[middle] > num) {
+                last = middle - 1;
+            }
+            else if (arr[middle] < num) {
+                start = middle + 1;
+            }
+            else {
+                last = middle;
+
+                if (arr[start] == arr[last]) {
+                    return start;
+                }
+            }
+        }
+       return -1;
+    }
+}
