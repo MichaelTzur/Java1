2ac4506609e7ab10f7380c7956074ebdc8a46299
MichaelTzur <59913971+MichaelTzur@users.noreply.github.com>
2020-02-05 11:08:13 +0200

    Add files via upload

---
+++ Question4.java
@@ -0,0 +1,41 @@
+public class Question4 {
+
+    public static void showMazePath(boolean[][] arr, int row, int col) {
+
+        System.out.println(row + ", " + col);
+        arr[row][col] = false;
+
+        if (row == arr.length - 1 && col == arr[0].length - 1) {
+            System.out.println("success!");
+
+            return;
+        }
+        //go down
+        if (row < arr.length - 1 && arr[row + 1][col]) {
+            showMazePath(arr, row + 1, col);
+        }
+        //go right
+        if (col < arr[0].length - 1 && arr[row][col + 1]) {
+            showMazePath(arr, row, col + 1);
+        }
+        //go up
+        if (row > 0 && arr[row - 1][col]) {
+            showMazePath(arr, row - 1, col);
+        }
+        //go left
+        if (col > 0 && arr[row][col - 1]) {
+            showMazePath(arr, row, col - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        boolean[][] arr = {{true, true, true, true ,true},
+                           {true, false, false, false, true},
+                           {true, false, true, true ,true},
+                           {true, false, true, false, false},
+                           {true, false, true, true ,true}};
+
+        showMazePath(arr, 0 , 0);
+    }
+}
