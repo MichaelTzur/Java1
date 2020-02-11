08297bb1b8d7c07a66b94af431242142e32659e0
MichaelTzur <59913971+MichaelTzur@users.noreply.github.com>
2020-02-11 22:27:09 +0200

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,57 @@
+public class Node {
+
+    private int value;
+    private Node next, previous, left, right;
+
+    public Node(int value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public Node() {
+    }
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+}
